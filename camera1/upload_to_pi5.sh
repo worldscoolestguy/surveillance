3ac315f4c7a6e4fb158430d9ae6d38eb288ce2fa
worldscoolestguy <mattprimmer@gmail.com>
2026-08-19 14:38:26 -0700

    Update upload_to_pi5.sh

--- camera1/upload_to_pi5.sh
+++ camera1/upload_to_pi5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 VIDEO_FILE="$MTX_SEGMENT_PATH"
-scp -o StrictHostKeyChecking=no "$VIDEO_FILE" matt@10.0.0.23:/var/www/html/videos/
+scp -o StrictHostKeyChecking=no "$VIDEO_FILE" YOUR_USERNAME@YOUR_PI5_IP:/var/www/html/videos/
 if [ $? -eq 0 ]; then
     rm "$VIDEO_FILE"
 fi
