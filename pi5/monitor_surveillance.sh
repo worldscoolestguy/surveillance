4f6dd15b2652a8eb31b8106b2954393a8db80e66
worldscoolestguy <mattprimmer@gmail.com>
2026-08-19 14:46:14 -0700

    Update monitor_surveillance.sh

--- pi5/monitor_surveillance.sh
+++ pi5/monitor_surveillance.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-inotifywait -m /home/matt/surveillance_drops -e close_write |
+inotifywait -m /home/YOUR_USERNAME/surveillance_drops -e close_write |
     while read path action file; do
         if [[ "$file" == *.mp4 && "$file" != event_* ]]; then
             echo "=== $(date) - Forwarding $file to Nano ==="
-            rsync -avz /home/matt/surveillance_drops/"$file" matt@10.0.1.69:/home/matt/videotransfer/
+            rsync -avz /home/YOUR_USERNAME/surveillance_drops/"$file" YOUR_USERNAME@YOUR_JETSON_IP:/home/YOUR_USERNAME/videotransfer/
         else
             echo "=== $(date) - Skipped event clip: $file ==="
         fi
